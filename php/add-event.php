acfc33e0adc4378d2663396dbcfa4796143aed25
Max Saperstone <max.saperstone@coveros.com>
2016-04-26 11:02:38 -0400

    Adding security checks to each post call

--- php/add-event.php
+++ php/add-event.php
@@ -1,5 +1,18 @@
 <?php
 
+session_name('shitLogin');
+// Starting the session
+
+session_set_cookie_params(2*7*24*60*60);
+// Making the cookie live for 2 weeks
+
+session_start();
+
+if (!isset ( $_SESSION ['id'] )) {
+    echo "You need to be logged in to perform this action";
+    exit();
+}
+
 require "sql.php";
 
 $new = $_POST ['data'];
