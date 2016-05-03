52a6387b92531efdbaac262f09ddec55efcbc0a5
Max Saperstone <max.saperstone@coveros.com>
2016-05-03 10:06:59 -0400

    Minifying js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var del = require('del');
 var cssmin = require('gulp-cssmin');
+var jsmin = require('gulp-jsmin');
 var rename = require('gulp-rename');
 
 gulp.task('less', function() {
@@ -10,15 +11,25 @@ gulp.task('less', function() {
                 console.log(err);
             })).pipe(gulp.dest('./css/'));
 });
-gulp.task('clean', function(cb) {
+gulp.task('clean-css', function(cb) {
     return del([ './css/*.min.css' ], cb);
 });
-gulp.task('cssmin', ['clean', 'less'], function() {
+gulp.task('clean-js', function(cb) {
+    return del([ './js/*.min.js' ], cb);
+});
+gulp.task('cssmin', ['clean-css', 'less'], function() {
     return gulp.src('./css/*.css').pipe(cssmin().on('error', function(err) {
         console.log(err);
     })).pipe(rename({
         suffix : '.min'
     })).pipe(gulp.dest('./css/'));
 });
+gulp.task('jsmin', ['clean-js'], function() {
+    return gulp.src('./js/*.js').pipe(jsmin().on('error', function(err) {
+        console.log(err);
+    })).pipe(rename({
+        suffix : '.min'
+    })).pipe(gulp.dest('./js/'));
+});
 
-gulp.task('default', ['cssmin']);
+gulp.task('default', ['cssmin', 'jsmin']);
--- header.php
+++ header.php
@@ -45,28 +45,28 @@
     <![endif]-->
     
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="js/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="js/classie.js"></script>
-    <script src="js/cbpAnimatedHeader.js"></script>
+    <script src="js/classie.min.js"></script>
+    <script src="js/cbpAnimatedHeader.min.js"></script>
 
     <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
+    <script src="js/jqBootstrapValidation.min.js"></script>
+    <script src="js/contact_me.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="js/freelancer.js"></script>
+    <script src="js/freelancer.min.js"></script>
     
     <!-- Moment -->
     <script src="js/moment.min.js"></script>
     
     <!-- Custom -->
     <link href="css/custom.min.css" rel="stylesheet" type="text/css">
-    <script src="js/custom.js"></script>
+    <script src="js/custom.min.js"></script>
 
 </head>
