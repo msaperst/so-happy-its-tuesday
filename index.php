f9aac608ac49564f86ad0699a6837aa79f38e156
Max Saperstone <max.saperstone@coveros.com>
2017-01-26 09:35:41 -0500

    Adding new image to homepage

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image:url('img/group.jpg'); background-position-x:50%; background-position-y:30%; min-height:550px;">
+    <header class="intro-header" style="background-image:url('img/halloween.jpg'); background-position-x:50%; background-position-y:30%; background-size:cover; min-height:550px;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
